b8f92fbdbd96d6da70720fcfe966cdb0655786dd
jaanus-pi <jaanus-soft@proton.me>
2024-01-12 16:46:26 +0200

    Gather and display data

--- shipments/src/pages/HomePage.jsx
+++ shipments/src/pages/HomePage.jsx
@@ -1,8 +1,41 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const HomePage = () => {
+  const [shipments, setShipments] = useState([]);
+
+  useEffect(() => {
+    fetch("https://my.api.mockaroo.com/shipments.json?key=5e0b62d0")
+      .then(res => res.json())
+      .then(json => setShipments(json));
+  }, []);
+
   return (
-    <div>HomePage</div>
+    <div>
+      <table>
+        <thead>
+        <tr>
+          <th>Order number</th>
+          <th>Date</th>
+          <th>Customer</th>
+          <th>Tracking number</th>
+          <th>Status</th>
+          <th>Consignee</th>
+        </tr>
+        </thead>
+        <tbody>
+        {shipments.map(shipment => 
+        <tr key={shipment.orderNo}>
+          <td>{shipment.orderNo}</td> 
+          <td>{shipment.date}</td>
+          <td>{shipment.customer}</td>
+          <td>{shipment.trackingNo}</td>
+          <td>{shipment.status}</td>
+          <td>{shipment.consignee}</td>
+        </tr>
+        )}
+        </tbody>
+      </table>
+    </div>
   )
 }
 
