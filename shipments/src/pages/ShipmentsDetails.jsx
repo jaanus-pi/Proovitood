bc54ce811390547723d958a4520a8784ce143360
jaanus-pi <jaanus-soft@proton.me>
2024-01-15 09:51:19 +0200

    Inputs, default value

--- shipments/src/pages/ShipmentsDetails.jsx
+++ shipments/src/pages/ShipmentsDetails.jsx
@@ -1,9 +1,15 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { useParams } from 'react-router-dom'
 
 const ShipmentsDetails = () => {
   const { index } = useParams();
   const [shipments, setShipments] = useState([]);
+  const orderNoRef = useRef();
+  const dateRef = useRef();
+  const customerRef = useRef();
+  const trackingNoRef = useRef();
+  const statusRef = useRef();
+  const consigneeRef = useRef();
 
   useEffect(() => {
     fetch("https://my.api.mockaroo.com/shipments.json?key=5e0b62d0")
@@ -15,12 +21,18 @@ const ShipmentsDetails = () => {
     <div>
       {shipments.length !== 0 &&
       <div>
-        {shipments[index].orderNo} <br />
-        {shipments[index].date} <br />
-        {shipments[index].customer} <br />
-        {shipments[index].trackingNo} <br />
-        {shipments[index].status} <br />
-        {shipments[index].consignee} <br />
+        <label>Order number</label> <br />
+        <input type="text" defaultValue={shipments[index].orderNo} ref={orderNoRef}/> <br />
+        <label>Date</label> <br />
+        <input type="text" defaultValue={shipments[index].date} ref={dateRef}/> <br />
+        <label>Customer</label> <br />
+        <input type="text" defaultValue={shipments[index].customer} ref={customerRef}/> <br />
+        <label>Tracking Number</label> <br />
+        <input type="text" defaultValue={shipments[index].trackingNo} ref={trackingNoRef}/> <br />
+        <label>Status</label> <br />
+        <input type="text" defaultValue={shipments[index].status} ref={statusRef}/> <br />
+        <label>Consignee</label> <br />
+        <input type="text" defaultValue={shipments[index].consignee} ref={consigneeRef}/> <br />
       </div>
       }
     </div>
